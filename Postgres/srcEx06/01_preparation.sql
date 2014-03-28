85598529ad832804fb8cc31afbf5ddc8e323babe
TheoWinter <theo.winter@gmail.com>
2014-03-28 10:06:01 +0100

    Initial commit for exercises in week 6

---
+++ Postgres/srcEx06/01_preparation.sql
@@ -0,0 +1,9 @@
+﻿/* Perparation for exercises week 6*/
+CREATE TABLE aa (a int primary key, b int[]);
+INSERT INTO aa VALUES (1, '{1,2,3,4}');
+INSERT INTO aa VALUES (2, ARRAY[1,2,3,4]);
+INSERT INTO aa VALUES (3, '{{1,2},{3,4}}');
+INSERT INTO aa VALUES (4, ARRAY[ARRAY[1,2],ARRAY[3,4]]);
+
+CREATE TABLE cc (a int, b char(2));
+INSERT INTO cc values (1, 'Aa'), (2, 'Bb'), (3, 'Cc'), (4, 'Dd'), (6, 'Ff');
