40dcc35ad18d44b71fbde95727608ca0bc3795ae
TheoWinter <theo.winter@gmail.com>
2014-03-28 12:07:59 +0100

    Solved Dictionaries/EAV exercises from week 6 (hstore)

--- Postgres/srcEx06/02_exercises.sql
+++ Postgres/srcEx06/02_exercises.sql
@@ -27,3 +27,41 @@ SELECT b[2:2][1:1] from aa where a=3;
 
 --7. Return the content of row 1 from table aa as independant values on rows
 SELECT unnest(b) from aa where a=1;
+
+
+
+/*
+ * Dictionaries/EAV Exercises:
+ * 1-5 - 28.03.2014
+*/
+--List all tables (NAE - Not An Exercise)
+select * from information_schema.tables
+
+--1. List
+--1.1 the first 10 alphabetic sorted, unique key in column "tags"
+SELECT distinct akeys(tags) as keys FROM planet_osm_point order by keys limit 10;
+
+--1.2 all keys in column "tags"
+SELECT distinct akeys(tags) as keys FROM planet_osm_point order by keys;
+
+--2. List
+--2.1 the first 10 alphabetic sorted, unique key-value pairs in column "tags"
+SELECT distinct hstore_to_matrix(tags) FROM planet_osm_point limit 10;
+
+--2.2 all sorted, unique key-value pairs in column "tags"
+SELECT distinct hstore_to_matrix(tags) FROM planet_osm_point;
+
+--3. Return the value of key name from cell osm_id=11210062 or osm_id=50075375
+SELECT tags->'name' FROM planet_osm_point where osm_id in (11210062,50075375);
+
+--4. Return all rows containg thekey parking
+SELECT tags->'name' FROM planet_osm_point WHERE tags ? 'parking';
+
+--5. Convert the field tags in tuple osm_id=50075375
+--5.1 into an array (option 1)
+--Note2Self: Alternativ --> hstore_to_array(hstore)
+SELECT %%tags FROM planet_osm_point where osm_id = 50075375;
+
+--5.2 into an array (option 2)
+--Note2Self: Alternativ --> hstore_to_matrix(hstore)
+SELECT %#tags FROM planet_osm_point where osm_id = 50075375;
