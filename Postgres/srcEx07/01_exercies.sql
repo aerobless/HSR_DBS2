9f794d2ef31f1b9813926bfa97104f568d242d7a
TheoWinter <theo.winter@gmail.com>
2014-04-04 10:51:08 +0200

    Solved exercise 2 of week 7 though without adjacency list

---
+++ Postgres/srcEx07/01_exercies.sql
@@ -0,0 +1,24 @@
+﻿/* Preparation: Load W05_graphs from http://wiki.hsr.ch/Datenbanken/files/W05_graphs.zip into postgres */
+-- 1.1 - Understand 4_queries.sql
+/* CTE sind wichtig für die Prüfung */
+
+/* Info: Alternative http://sqlfiddle.com/#!12/54f31/7 */
+-- 2.0 - Select all employees from "Angestellter" and select all "Top Chef" from "Angestellter"
+SELECT persnr, name, chef FROM angestellter ORDER BY 1;
+SELECT persnr, name, chef FROM angestellter WHERE chef is null order by 1;
+
+-- 2.1 - Write a query that selects all "chef"s (boss).
+SELECT cheftab.chef, angtab.name, count(*) AS underlings 
+FROM angestellter AS angtab
+	INNER JOIN angestellter AS cheftab ON cheftab.chef = angtab.persnr
+GROUP BY cheftab.chef, angtab.name ORDER BY 1;
+
+-- 2.2 - Write a query that selects all underlings and ignores all chefs (bosses).
+select distinct angtab.persnr, angtab.name, angtab.chef
+FROM angestellter AS angtab
+	LEFT OUTER JOIN angestellter AS cheftab ON cheftab.chef = angtab.persnr
+WHERE cheftab.persnr is null
+ORDER BY 1;
+
+-- 2.3 - Write a query that selects all underlings of chef (Steiner 1010)
+SELECT persnr, name, chef FROM angestellter WHERE chef=1001 ORDER BY 1;
